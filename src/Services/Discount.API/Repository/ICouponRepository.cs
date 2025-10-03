0e80ae997564fa3ddc43fc6ad400ea03a96b44b4
Md. Hasibul Islam <hasibsanto0@gmail.com>
2025-10-03 23:26:55 +0600

    Update Discount.API work

---
+++ src/Services/Discount.API/Repository/ICouponRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.API.Models;
+
+namespace Discount.API.Repository
+{
+    public interface ICouponRepository
+    {
+        Task<Coupon> GetDiscount(string ProductId);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string ProductId);
+
+    }
+}
